d7e9bae21c2133be53f2a840095872724e0e59cc
Sergei Kovalev <skovalev1902@gmail.com>
2022-08-21 17:33:24 +0300

    r

---
+++ TestAppw/ViewModels/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  TestAppw
+//
+//  Created by Sergei Kovalev on 21.08.2022.
+//
+
+import Foundation
---
+++ TestAppw/ViewModels/ProductDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailsViewModel.swift
+//  TestAppw
+//
+//  Created by Sergei Kovalev on 21.08.2022.
+//
+
+import Foundation
